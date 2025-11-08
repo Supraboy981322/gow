28df3cb46fc8622cda6addc806b8bb7784bbc67f
Zane <97927259+Supraboy981322@users.noreply.github.com>
2025-11-08 15:29:12 -0600

    init commit

---
+++ libs.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"errors"
+)
+
+type (
+	Writer struct{}
+)
+var (
+	wr = Writer{}
+)
+
+func (w Writer) l(a ...interface{}) {
+	fmt.Println(a...)
+}
+
+func (w Writer) f(str string, a ...interface{}) {
+	fmt.Printf(str, a...)
+}
+
+func (w Writer) sf(str string, a ...interface{}) {
+	fmt.Sprintf(str, a...)
+}
+
+func (w Writer) s(a ...interface{}) {
+	fmt.Sprint(a...)
+}
+
+func (w Writer) mkerr(a string) error {
+	return errors.New(a)
+}
+
+func (w Writer) mkerrf(str string, a ...interface{}) {
+	fmt.Errorf(str, a...)
+}
+
+func (w Writer) err(a ...interface{}) {
+	fmt.Fprint(os.Stderr, a...)
+}
+
+func (w Writer) errl(a ...interface{}) {
+	fmt.Fprintln(os.Stderr, a...)
+}
---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"os"
+	"strings"
+//	"net/http"
+)
+
+type (
+	InvalidArg struct {
+		Value int
+		Exists bool
+	}
+)
+
+var (
+	args = os.Args[1:]
+	silent = false
+	url string
+	invalidArg InvalidArg
+)
+
+func main() {
+	strRemaining := false
+	for i := 0; i < len(args); i++ {
+		curArg := args[i]
+		wr.l(curArg)
+		if !strRemaining {
+			switch curArg[0] {
+			case '-':
+				strSplit := strings.Split(curArg, "")
+				strRemaining = readArgChars(strSplit)
+				if invalidArg.Exists {
+					errOut(wr.mkerr("arg"), curArg, invalidArg.Value)
+				}
+			default:
+				url = curArg
+			}
+		} else {
+			url += curArg
+		}
+	}
+}
+
+func readArgChars(arg []string) bool { 
+	strRemaining := false
+	for i := 0; i < len(arg); i++ {
+		switch (arg[i]) {
+		case "-":
+			strRemaining = true
+			break
+		case "s":
+			silent = true
+		default:
+			invalidArg.Value = i
+			invalidArg.Exists = true
+			return strRemaining
+		}
+	}
+	return strRemaining
+}
+
+func errOut(err error, str any, ext ...interface{}) {
+	if index, ok := ext[0].(int); ok && err.Error() == "arg" {
+		var pointer string
+		for i := 0; i < index; i++ {
+			pointer += " "
+		}
+		pointer += "\033[1;31m^\033[0m"
+		str, ok := str.(string)
+		if !ok {
+			wr.errl("failed to convert str type of any to type of string in errOut()")
+			os.Exit(1)
+		}
+
+		str = str[:index] + "\033[1;31m" +
+					string(str[index]) +
+					"\033[0m" + str[index+1:]
+
+		wr.errl("invalid arg:\n" +
+						"  " + str + "\n" +
+			      "  " + pointer + "\n")
+	} else {
+		
+	}
+}
